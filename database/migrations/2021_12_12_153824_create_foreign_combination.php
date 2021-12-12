9058d2b026fcbb9c41db0e5e5ec3f4de0b041353
Leonardo Costa <88503342+leonardocrcst@users.noreply.github.com>
2021-12-12 14:26:33 -0300

    Migrações, seeders e factories

---
+++ database/migrations/2021_12_12_153824_create_foreign_combination.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateForeignCombination extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $entity = 'combinations';
+        if (Schema::hasTable($entity)) {
+            Schema::create($entity, function (Blueprint $table) {
+                $table->foreign('user', 'combination-user_x_users-id')->on('users')->references('id');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('foreign_combination');
+        Schema::dropForeign('combination-user_x_users-id');
+    }
+}
